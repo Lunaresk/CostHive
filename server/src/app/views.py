d03bcc4e664f310ee21b10e66afb5933beff28ae
Lunaresk <lingsonb@gmail.com>
2022-01-17 23:04:26 +0100

    fix: floating-point error

--- server/src/app/views.py
+++ server/src/app/views.py
@@ -91,7 +91,9 @@ def group_results(results: tuple) -> dict:
             result_dict[result[0]][str(result[1])] = {}
         result_dict[result[0]][str(result[1])][result[2]] = (
             result[3], result[4])
-        amount, price = int(result[3]), float(result[4].split(" ")[0].replace(",", "."))
-        result_dict[result[0]]["sum"] += (amount * (price * 100)) / 100
+        price = int(result[3]) * int(float(result[4].split(" ")[0].replace(",", "."))*100)
+        result_dict[result[0]]["sum"] += price
+    for key in result_dict.keys():
+        result_dict[key]["sum"] /= 100
     LOGGER.debug("Grouped.")
     return result_dict
