8e17a2fb259cea0478e893727d955077d8dc9010
Lunaresk <lingsonb@gmail.com>
2023-06-20 00:01:30 +0200

    minor: function for testing purposes Reinserted the testfunction

--- backend/src/main/routes.py
+++ backend/src/main/routes.py
@@ -1,9 +1,18 @@
-from flask import jsonify
+from flask import jsonify, request
 from . import bp
 from src.models import User
+from src.models.schemas import UserSchema
 from src.utils.routes_utils import render_custom_template as render_template
 
 @bp.route('/')
 @bp.route('/index')
 def index():
     return render_template("base.html")
+
+@bp.route('/test')
+def test():
+    print(request.content_type)
+    user_objects = User.query.all()
+    schema = UserSchema(many = True)
+    users = schema.dump(user_objects)
+    return jsonify(users)
